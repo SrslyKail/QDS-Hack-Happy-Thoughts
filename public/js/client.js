5cb740322124a90a67c36a1131311ae5bc42d086
Corey Buchan <xouwan021592@gmail.com>
2024-03-08 23:46:03 -0800

    Add client.js to index. Add "onClick" to cards.

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,7 @@
 
-console.log("Client script loaded.");
+//console.log("Client script loaded.");
+
+const cardArea = document.getElementById("cardArea");
 
 // a function declaration inside of a callback ... which takes a callback function :O
 function ajaxGET(url, callback) {
@@ -23,4 +25,20 @@ function ajaxGET(url, callback) {
   xhr.open("GET", url); // localhost:8000/weekdays?format=html
   xhr.send();
 
+}
+
+
+// Setup for each card to be clickable
+document.querySelectorAll(".card").forEach(function (currentElement, currentIndex, listObj) {
+  //console.log(currentElement, currentIndex, listObj);
+  currentElement.addEventListener("click", onCardClick);
+})
+
+/**
+ * Is called when a card is clicked.
+ * @param {PointerEvent} e The event that called this function.
+ * @returns {void}
+ */
+function onCardClick(e) {
+    console.log(e);
 }
