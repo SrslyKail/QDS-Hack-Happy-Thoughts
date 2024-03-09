0f13a5ff84af2fd9de1290a267c1a0c6797d7aba
Corey Buchan <xouwan021592@gmail.com>
2024-03-08 17:57:55 -0800

    initialize repo layout

---
+++ public/js/client.js
@@ -0,0 +1,31 @@
+
+
+console.log("Client script loaded.");
+
+// a function declaration inside of a callback ... which takes a callback function :O
+function ajaxGET(url, callback) {
+
+    const xhr = new XMLHttpRequest();
+
+    //console.log("xhr", xhr);
+    xhr.onload = function() {
+        value = this.responseText;
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            //console.log('responseText:' + xhr.responseText);
+
+            // callback function
+            value = this.responseText;
+            callback(this.responseText);
+
+        } else {
+            console.log(this.status);
+        }
+    }
+    xhr.open("GET", url); // localhost:8000/weekdays?format=html
+    xhr.send();
+
+}
+
+
+
+
