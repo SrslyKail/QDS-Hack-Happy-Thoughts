bf26c0219267603be9225cd920aa2b7e7e7b8b9a
Polina Omelyantseva <pomelyantseva@my.bcit.ca>
2024-03-09 22:08:07 -0800

    navbar links work on pc, not on mobile

--- public/js/client.js
+++ public/js/client.js
@@ -376,23 +376,3 @@ async function updateModalBody() {
 }
 
 updateModalBody();
-
-//navbar links
-document.querySelector("#navbarPlaceholder").addEventListener("mousedown", function (e) {
-  document.querySelector(".nav-item-1").addEventListener('click', function (e) {
-    window.open("./index.html", "_self");
-    //console.log("1 works");
-  })
-  document.querySelector(".nav-item-2").addEventListener('click', function (e) {
-    window.open("/about.html", "_self");
-    //console.log("2 works");
-  })
-  document.querySelector(".nav-item-3").addEventListener('click', function (e) {
-    window.open("/terms.html", "_self");
-    //console.log("3 works");
-  })
-  document.querySelector(".nav-item-4").addEventListener('click', function (e) {
-    window.open("/resources.html", "_self");
-    //console.log("4 works");
-  })
-});
